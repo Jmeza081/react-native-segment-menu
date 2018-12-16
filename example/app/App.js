63f968033017fad2f8a606442210158bf3762e36
Jmeza081 <jmeza081@gmail.com>
2018-12-16 14:10:38 -0600

    clean up code for readability

--- example/app/App.js
+++ example/app/App.js
@@ -1,31 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    ImageBackground,
-    StatusBar,
-} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, StatusBar } from 'react-native';
 import SegmentMenu from 'react-native-segment-menu';
+import HeaderBar from './components/HeaderBar';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as Animatable from 'react-native-animatable';
 import { palette } from './config/colors';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
+const BACKGROUND_IMAGE =
+    'https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showSegmentMenu: false,
+        };
+    }
+
+    getMenuTriggerComponent = (materialIconName, MenuName) => {
+        return (
+            <View
+                style={{
+                    flexDirection: 'column',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
+                    paddingBottom: 8,
+                }}
+            >
+                <Icon name={materialIconName} size={25} color={palette.neutral.white} />
+                <Text style={styles.menuTriggerText}>{MenuName}</Text>
+            </View>
+        );
+    };
+
+    toggleMenuVisibility = () => {
+        if (this.state.showSegmentMenu) {
+            this.segmentMenu.fadeOutUp(500).then(() => {
+                this.setState({
+                    showSegmentMenu: false,
+                });
+            });
+        } else {
+            this.setState({
+                showSegmentMenu: true,
+            });
+        }
+    };
+
+    renderWrappedStatusBar = () => {
+        return (
+            <View style={styles.statusBar}>
+                <StatusBar backgroundColor="blue" barStyle="light-content" />
+            </View>
+        );
+    };
+
     render() {
+        let { showSegmentMenu } = this.state;
+
         const myProjects = (
             <View
                 style={{
@@ -34,165 +67,27 @@ export default class App extends Component<{}> {
                     alignItems: 'center',
                 }}
             >
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'flex-start',
-                        bottom: 20,
-                    }}
-                >
-                    <Text style={{ color: 'white' }}>My Projects</Text>
-                </View>
-
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                    }}
-                >
-                    <View
-                        style={{
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            flex: 1,
-                        }}
-                    >
-                        <Text style={{ color: 'white', padding: 5 }}>shopping cart</Text>
-                        <Text style={{ color: 'white', padding: 5 }}>Submissions</Text>
-                    </View>
-
-                    <View
-                        style={{
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            flex: 1,
-                        }}
-                    >
-                        <Text style={{ color: 'white', padding: 5 }}>shopping</Text>
-                        <Text style={{ color: 'white', padding: 5 }}>Submissions</Text>
-                    </View>
-                </View>
+                <Text style={styles.menuTriggerText}>SubMenu</Text>
             </View>
         );
 
         const dataTest = [
             {
-                menuTrigger: (
-                    <View
-                        style={{
-                            flexDirection: 'column',
-                            justifyContent: 'space-around',
-                            alignItems: 'center',
-                            paddingBottom: 8,
-                        }}
-                    >
-                        <Icon name={'clock'} size={25} color={palette.neutral.white} />
-                        <Text
-                            style={{
-                                color: '#FFFFFF',
-                                fontSize: 10,
-                                paddingTop: 5,
-                                textAlign: 'center',
-                                fontFamily: 'Lora-Regular',
-                            }}
-                        >
-                            5 Minute Reads
-                        </Text>
-                    </View>
-                ),
+                menuTrigger: this.getMenuTriggerComponent('clock', '5 Minute Reads'),
                 subMenuComponent: myProjects,
             },
             {
-                menuTrigger: (
-                    <View
-                        style={{
-                            flexDirection: 'column',
-                            justifyContent: 'space-around',
-                            alignItems: 'center',
-                            paddingBottom: 8,
-                        }}
-                    >
-                        <Icon
-                            name={'food-apple'}
-                            size={25}
-                            color={palette.neutral.white}
-                        />
-                        <Text
-                            style={{
-                                color: '#FFFFFF',
-                                fontSize: 10,
-                                paddingTop: 5,
-                                textAlign: 'center',
-                                fontFamily: 'Lora-Regular',
-                            }}
-                        >
-                            Fruit Only
-                        </Text>
-                    </View>
-                ),
+                menuTrigger: this.getMenuTriggerComponent('food-apple', 'Fruit Only'),
                 subMenuComponent: myProjects,
             },
             {
-                menuTrigger: (
-                    <View
-                        style={{
-                            flexDirection: 'column',
-                            justifyContent: 'space-around',
-                            alignItems: 'center',
-                            paddingBottom: 8,
-                        }}
-                    >
-                        <Icon
-                            name={'food-croissant'}
-                            size={25}
-                            color={palette.neutral.white}
-                        />
-                        <Text
-                            style={{
-                                color: '#FFFFFF',
-                                fontSize: 10,
-                                paddingTop: 5,
-                                textAlign: 'center',
-                                fontFamily: 'Lora-Regular',
-                            }}
-                        >
-                            Dessert
-                        </Text>
-                    </View>
-                ),
+                menuTrigger: this.getMenuTriggerComponent('food-croissant', 'Desserts'),
                 subMenuComponent: myProjects,
             },
             {
-                menuTrigger: (
-                    <View
-                        style={{
-                            flexDirection: 'column',
-                            justifyContent: 'space-around',
-                            alignItems: 'center',
-                            paddingBottom: 8,
-                        }}
-                    >
-                        <Icon
-                            name={'bottle-wine'}
-                            size={25}
-                            color={palette.neutral.white}
-                        />
-                        <Text
-                            style={{
-                                color: '#FFFFFF',
-                                fontSize: 10,
-                                paddingTop: 5,
-                                textAlign: 'center',
-                                fontFamily: 'Lora-Regular',
-                            }}
-                        >
-                            Great With Wine
-                        </Text>
-                    </View>
+                menuTrigger: this.getMenuTriggerComponent(
+                    'bottle-wine',
+                    'Great With Wine',
                 ),
                 subMenuComponent: myProjects,
             },
@@ -201,79 +96,47 @@ export default class App extends Component<{}> {
         return (
             <ImageBackground
                 source={{
-                    uri:
-                        'https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                    uri: BACKGROUND_IMAGE,
                 }}
                 opacity={0.5}
-                style={[{ flex: 1 }, { backgroundColor: 'black' }]}
+                style={styles.imageBackground}
             >
-                <View style={{ height: 20, backgroundColor: palette.surface }}>
-                    <StatusBar backgroundColor="blue" barStyle="light-content" />
-                </View>
-                <View
-                    style={{
-                        height: 60,
-                        backgroundColor: palette.surface,
-                        justifyContent: 'center',
-                        shadowColor: palette.neutral.black,
-                        shadowOffset: { width: 0, height: 3 },
-                        shadowOpacity: 1,
-                        shadowRadius: 3,
-                        zIndex: 1,
-                    }}
-                >
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            marginHorizontal: 10,
-                        }}
+                {this.renderWrappedStatusBar()}
+                <HeaderBar onSilverwarePress={this.toggleMenuVisibility} />
+                {showSegmentMenu && (
+                    <Animatable.View
+                        animation={'fadeInDown'}
+                        duration={500}
+                        ref={(ref) => (this.segmentMenu = ref)}
                     >
-                        <Text
-                            style={{
-                                color: palette.neutral.white,
-                                fontFamily: 'Merriweather-BoldItalic',
-                            }}
-                        >
-                            Foodinary
-                        </Text>
-                        <Icon
-                            name={'silverware'}
-                            color={palette.neutral.white}
-                            size={25}
+                        <SegmentMenu
+                            data={dataTest}
+                            segmentPointerColor={palette.accent}
+                            subMenuBackgroundColor={palette.surface}
+                            parentMenuBackgroundColor={palette.surface}
                         />
-                    </View>
-                </View>
-
-                <View style={{ flexDirection: 'column', alignItems: 'center' }}>
-                    <SegmentMenu
-                        data={dataTest}
-                        segmentPointerColor={palette.accent}
-                        subMenuBackgroundColor={palette.surface}
-                        parentMenuBackgroundColor={palette.surface}
-                    />
-                </View>
+                    </Animatable.View>
+                )}
             </ImageBackground>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    imageBackground: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: palette.neutral.black,
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    statusBar: {
+        height: 20,
+        backgroundColor: palette.surface,
+        zIndex: 2,
     },
-    instructions: {
+    menuTriggerText: {
+        color: palette.neutral.white,
+        fontSize: 10,
+        paddingTop: 5,
         textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+        fontFamily: 'Lora-Regular',
     },
 });
