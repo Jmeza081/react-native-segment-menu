e30e2e7e95998022e41db30461f66be0522151e9
Jmeza081 <jmeza081@gmail.com>
2018-12-17 19:48:08 -0600

    add dummy recipies

---
+++ example/app/config/dummyCardData.js
@@ -0,0 +1,36 @@
+const dummyRecipes = [
+    {
+        media:
+            'https://images.unsplash.com/photo-1488900128323-21503983a07e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+        prepTimeMinutes: 45,
+        recipeTitle: 'Feeling Dessert?',
+        recipeDescription:
+            'Wanna eat something sweet without breaking the bank (or your calorie intake), try this recipe now!',
+    },
+    {
+        media:
+            'https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        prepTimeMinutes: 40,
+        recipeTitle: 'Why this burger will change your life',
+        recipeDescription:
+            'Passed down in my family for generations, I guarantee this burger recipe will change your life!',
+    },
+    {
+        media:
+            'https://images.unsplash.com/photo-1506802913710-40e2e66339c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+        prepTimeMinutes: 5,
+        recipeTitle: 'This morning smoothie will start your day right',
+        recipeDescription:
+            'Ever gotten off on the wrong foot? This smoothie is a sure fire way to set you on the right path!',
+    },
+    {
+        media:
+            'https://images.unsplash.com/photo-1543363136-3fdb62e11be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',
+        prepTimeMinutes: 15,
+        recipeTitle: '15 Yogurts that blow your socks off!',
+        recipeDescription:
+            'If you dont like these 15 recipes that were hand-picked by our staff, I will personally eat my hat!',
+    },
+];
+
+export default dummyRecipes;
