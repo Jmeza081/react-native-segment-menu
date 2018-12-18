69241a7b5245a3cab6554c36615d3792c5803231
Jmeza081 <jmeza081@gmail.com>
2018-12-17 19:48:35 -0600

    make recipe card class

--- example/app/components/RecipeCard/RecipeCard.js
+++ example/app/components/RecipeCard/RecipeCard.js
@@ -1,38 +1,111 @@
-import React from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, TouchableWithoutFeedback } from 'react-native';
 import PropTypes from 'prop-types';
 import { styles } from './styles';
+import { palette } from '../../config/colors';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as Animatable from 'react-native-animatable';
 
-const RecipeCard = (props) => {
-    return (
-        <View style={styles.imageCard}>
-            <ImageBackground
-                source={{
-                    uri:
-                        'https://images.unsplash.com/photo-1543363136-3fdb62e11be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',
-                }}
-                style={styles.articleImage}
-            >
-                <View style={styles.prepTimeBadge}>
-                    <Text>PrepTime Badge</Text>
+export default class App extends Component {
+    static propTypes = {
+        recipeCardData: PropTypes.shape({
+            media: PropTypes.string.isRequired,
+            prepTimeMinutes: PropTypes.number.isRequired,
+            recipeTitle: PropTypes.string.isRequired,
+            recipeDescription: PropTypes.string.isRequired,
+        }),
+    };
+
+    static defaultProps = {
+        recipeCardData: {
+            media:
+                'https://images.unsplash.com/photo-1543363136-3fdb62e11be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',
+            prepTimeMinutes: 15,
+            recipeTitle: '15 Yogurts that blow your socks off!',
+            recipeDescription:
+                'If you dont like these 15 recipes that were hand-picked by our staff, I will personally eat my hat!',
+        },
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            favorited: false,
+            addedToMyFoods: false,
+        };
+    }
+
+    toggleFavorited = () => {
+        this.setState((prevState) => ({
+            favorited: !prevState.favorited,
+        }));
+
+        this.myFavorites.rubberBand(1000);
+    };
+
+    toggleAddedToList = () => {
+        this.setState((prevState) => ({
+            addedToMyFoods: !prevState.addedToMyFoods,
+        }));
+
+        this.myFoods.rubberBand(1000);
+    };
+
+    render() {
+        let { recipeCardData } = this.props;
+        let { favorited, addedToMyFoods } = this.state;
+
+        return (
+            <View style={styles.imageCard}>
+                <ImageBackground
+                    source={{
+                        uri: recipeCardData.media,
+                    }}
+                    style={styles.articleImage}
+                >
+                    <View style={styles.prepTimeBadge}>
+                        <Text style={styles.prepTimeText}>
+                            Prep-Time: {recipeCardData.prepTimeMinutes} mins
+                        </Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.cardHeader}>
+                    <Text style={styles.articleHeader}>{recipeCardData.recipeTitle}</Text>
+                    <Text style={styles.articleSubHeader}>
+                        {recipeCardData.recipeDescription}
+                    </Text>
                 </View>
-            </ImageBackground>
-            <View style={styles.cardHeader}>
-                <Text>15 Yogurts that blow your socks off!</Text>
-                <Text>
-                    If you dont like these 15 recipes that were hand-picked by our staff,
-                    I will personally eat my hat!
-                </Text>
+                <View style={styles.actionArea}>
+                    <TouchableWithoutFeedback onPress={this.toggleFavorited}>
+                        <Animatable.View
+                            ref={(ref) => (this.myFavorites = ref)}
+                            style={styles.iconContainer}
+                        >
+                            <Icon
+                                name={'heart'}
+                                size={20}
+                                color={favorited ? palette.accent : palette.midTone}
+                            />
+                        </Animatable.View>
+                    </TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback
+                        onPress={this.toggleAddedToList}
+                        style={styles.iconContainer}
+                    >
+                        <Animatable.View
+                            ref={(ref) => (this.myFoods = ref)}
+                            style={styles.iconContainer}
+                        >
+                            <Icon
+                                name={'silverware-fork-knife'}
+                                size={20}
+                                color={addedToMyFoods ? palette.accent : palette.midTone}
+                            />
+                        </Animatable.View>
+                    </TouchableWithoutFeedback>
+                </View>
             </View>
-            <View style={styles.actionArea}>
-                <Text>Fav</Text>
-                <Text>Add</Text>
-            </View>
-        </View>
-    );
-};
-RecipeCard.propTypes = {};
-
-RecipeCard.defaultProps = {};
-
-export default RecipeCard;
+        );
+    }
+}
--- example/app/components/RecipeCard/styles.js
+++ example/app/components/RecipeCard/styles.js
@@ -7,24 +7,56 @@ const styles = StyleSheet.create({
     imageCard: {
         height: 300,
         width: width,
+        paddingVertical: '1%',
     },
     articleImage: {
         flex: 3,
     },
     cardHeader: {
         flex: 1,
-        backgroundColor: 'red',
+        backgroundColor: palette.surface,
     },
     actionArea: {
-        backgroundColor: 'blue',
+        flex: 0.5,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        backgroundColor: palette.surface,
+    },
+    iconContainer: {
+        paddingHorizontal: '2%',
     },
     prepTimeBadge: {
+        top: '6%',
+        paddingVertical: 5,
         backgroundColor: palette.accent,
         maxWidth: 150,
         shadowColor: palette.neutral.black,
         shadowOffset: { width: 0, height: 3 },
         shadowOpacity: 1,
         shadowRadius: 3,
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+    },
+    prepTimeText: {
+        paddingLeft: '5%',
+        color: palette.neutral.white,
+        fontFamily: 'Lora-Italic',
+        fontWeight: 'bold',
+    },
+    articleHeader: {
+        paddingTop: '2%',
+        paddingLeft: '2%',
+        color: palette.neutral.white,
+        fontFamily: 'Merriweather-Bold',
+        fontSize: 13,
+    },
+    articleSubHeader: {
+        paddingTop: '1%',
+        paddingLeft: '2%',
+        color: palette.neutral.white,
+        fontFamily: 'Merriweather-Regular',
+        fontSize: 10,
     },
 });
 
